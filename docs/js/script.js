12498a74144fc2050faaf8888a84b41ccd337609
Taylor Anderson <taylorranderson@gmail.com>
2021-07-26 15:59:40 -0700

    current weather now displays

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,8 +1,16 @@
 var apiKey = "44d934bc3830ac3f23662672055e4269";
 var userFormEl = document.querySelector('#user-form');
-var cityInputEl = document.querySelector('#city');
-var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 var historyEl = document.querySelector("#searchHistory");
+var cityInputEl = document.querySelector('#city');
+var weatherSearchTerm = document.querySelector("#weather-search-term");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+var tempContainerEl = document.querySelector("#temp-container");
+var windContainerEl = document.querySelector("#wind-container");
+var humidityContainerEl = document.querySelector("#humidity-container");
+var uvContainerEl = document.querySelector("#uv-container");
+
+
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -21,10 +29,10 @@ var getWeather = function (cityName) {
   fetch(apiURL)
     .then(function (response) {
       if(response.ok) {
-        console.log(response);
+        // console.log(response);
       response.json().then(function(data){
-        console.log(data);
-
+        // console.log(data);
+        displayWeather(data, cityName);
       });
       } else {
         alert("Error: " + response.statusText);
@@ -34,6 +42,46 @@ var getWeather = function (cityName) {
       alert("Unable to connect to open weather");
     });
 };
+
+var displayWeather = function(weatherData, cityName) {
+  var currentDate = moment().format("(M/D/Y)");
+  weatherSearchTerm.textContent = cityName + " " + currentDate;
+  console.log(weatherData);
+  var temp = "Temp: " + ((weatherData.main.temp - 273.15) * 1.8 + 32).toFixed(2) + "℉";
+  var wind = "Wind: " + weatherData.wind.speed + " MPH";
+  var humidity = "Humidity :" + weatherData.main.humidity + "%";
+
+  var lat = weatherData.coord.lat;
+  var lon = weatherData.coord.lon;
+
+  var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  fetch(apiURL)
+  .then(function (response) {
+    if(response.ok) {
+      // console.log(response);
+      response.json().then(function(data){
+        // console.log(data);
+        displayUV(data);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+.catch(function(error) {
+  alert("Unable to connect to open weather");
+});
+
+  tempContainerEl.textContent = temp;
+  windContainerEl.textContent = wind;
+  humidityContainerEl.textContent = humidity;  
+};
+
+var displayUV = function(weatherData) {
+  console.log(weatherData);
+  var uv = "UV Index: " + weatherData.current.uvi;
+
+  uvContainerEl.textContent = uv;
+}
 
 function renderSearchHistory() {
   historyEl.innerHTML = "";
